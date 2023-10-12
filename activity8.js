893e3a367aa087fa1c55444044f231832fcfceee
JherannKaryllePascual <145414565+JherannKaryllePascual@users.noreply.github.com>
2023-10-12 06:37:54 +0530

    Add files via upload

---
+++ activity8.js
@@ -0,0 +1,13 @@
+const favSingers = ['Swift', 'Dion', 'Grande'];
+console.log(favSingers);
+
+const favNumbers = ['21', '6', '12', '20'];
+console.log(favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
